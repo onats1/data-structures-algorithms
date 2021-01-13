e19a145ee54cd7823b31107b40153c54c6d18268
onats1 <donatemowo@yahoo.com>
2021-01-13 20:07:53 +0100

    Finished solving anagram problem.

--- src/FrequencyCounter.kt
+++ src/FrequencyCounter.kt
@@ -13,7 +13,7 @@ fun same(arr1: ArrayList<Int>, arr2: ArrayList<Int>): Boolean {
     if (arr1.size != arr2.size) {
         return false
     }
-    arr1.forEachIndexed { position, number ->
+    arr1.forEachIndexed { _, number ->
         val correctIndex = arr2.indexOf(number.pow())
         if (correctIndex == -1) {
             return false
@@ -23,7 +23,7 @@ fun same(arr1: ArrayList<Int>, arr2: ArrayList<Int>): Boolean {
     return true
 }
 
-// Optimized solution using frequency conter method.
+// Optimized solution using frequency conter method. O(n)
 fun sameOptimized(arr1: ArrayList<Int>, arr2: ArrayList<Int>): Boolean {
     if (arr1.size != arr2.size) {
         return false
@@ -39,10 +39,39 @@ fun sameOptimized(arr1: ArrayList<Int>, arr2: ArrayList<Int>): Boolean {
         frequencyCounter2[number] = frequencyCounter2[number]?:0 + 1
     }
     frequencyCounter1.forEach { (key, value) ->
+        if (!frequencyCounter2.containsKey(key.pow())) {
+            return false
+        }
         if (frequencyCounter2[key.pow()] != value) {
             return false
         }
     }
+    return true
+}
+
+/**
+ * Given two strings, write a function to determine if the second string is an anagram of the first string.
+ *
+ * */
+fun isAnagram(firstWord: String, secondWord: String): Boolean{
+    if (firstWord.length != secondWord.length) {
+        return false
+    }
+    val frequencyCounter1 = hashMapOf<Char, Int>()
+    val frequencyCounter2 = hashMapOf<Char, Int>()
+
+    firstWord.toLowerCase().forEach { letter ->
+        frequencyCounter1[letter] = frequencyCounter1[letter]?:0 + 1
+    }
+    secondWord.toLowerCase().forEach { letter ->
+        frequencyCounter2[letter] = frequencyCounter2[letter]?:0 + 1
+    }
+
+    frequencyCounter1.forEach { (key, value) ->
+        if (!frequencyCounter2.containsKey(key)) return false
+        if (frequencyCounter2[key] != value) return false
+    }
+
     return true
 }
 
@@ -52,4 +81,9 @@ fun main() {
 
     val result = sameOptimized(firstArray, secondArray)
     println(result)
+
+    val firstword = "Iceman"
+    val secondWord = "Cinema"
+
+    println(isAnagram(firstword, secondWord))
 }
